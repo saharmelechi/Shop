d8ebe57825c7128f4afce0206749678130d7d5f6
sahar <saharmelechi22@gmail.com>
2018-11-23 10:00:34 +0200

    Sahar- Added models and controlles unfinished.

---
+++ Shop/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+
+    }
+}
---
+++ Shop/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Models
+{
+    public class Product
+    {
+
+        public int ProductID { get; set; }
+
+        public int Quantity { get; set; }
+
+        //public int Category { get; set; }
+
+        public double Price { get; set; }
+
+        public string Name{ get; set; }
+
+    }
+}
